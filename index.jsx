fb5043a599f2cc906db14d4b225bf9bde7278a60
Juan122113 <ammiel89@hotmail.com>
2025-11-07 19:45:40 -0300

    Unique commit

---
+++ index.jsx
@@ -0,0 +1,24 @@
+const { useState } = React;
+
+export const ColorPicker = () => {
+
+  const initialValue = "#ffffff"
+
+  const [isColor, setIsColor] = useState("#ffffff");
+
+  const defaultStyles = {
+    backgroundColor: isColor,
+    color: isColor
+  }
+
+  const handleInputChange = (e) => {
+    setIsColor(e.target.value
+   );
+  } 
+
+  return (
+    <div id="color-picker-container" style={defaultStyles} >
+      <input id="color-input" type="color" onChange={handleInputChange} value={isColor}/>
+    </div>
+  );
+};
